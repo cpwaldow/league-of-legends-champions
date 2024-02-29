513e1e3f11d2504d97652908059c28dff012e856
Carlos Eduardo <cpwaldow@gmail.com>
2024-02-28 23:10:48 -0300

    fetchAPI

--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -1,11 +1,7 @@
-import { handleChampionsList } from '../utils/handleChampionsList';
-
 export const fetchChampionsList = async () => {
-  const response = await fetch(
-    'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json',
-  );
+  const API_URL =
+    'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json';
+  const response = await fetch(API_URL);
   const data = await response.json();
-
-  const handleData = handleChampionsList(data.data);
-  return handleData;
+  return data;
 };
