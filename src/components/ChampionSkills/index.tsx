a61baa101239e427b0342b38ed7acc6fa26beb4f
Carlos Eduardo <cpwaldow@gmail.com>
2024-03-05 15:23:33 -0300

    adicionando imagem para passiva e habilidades

--- src/components/ChampionSkills/index.tsx
+++ src/components/ChampionSkills/index.tsx
@@ -9,6 +9,7 @@ type PropsType = {
       cost: number[];
       range: number[];
       id: string;
+      image: any;
       [key: string]: string | number | number[];
     }[];
     resourse: string;
@@ -18,30 +19,40 @@ type PropsType = {
 
 const ChampionSkills = ({ skills }: PropsType) => {
   const abilityKey = ['Q', 'W', 'E', 'R'];
+  const PASSIVE_URL_IMG = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/passive/${skills.passive.image.full}`;
 
   return (
     <>
       <h3>Habilidades</h3>
       <div className='championsSkill__spells'>
-        <p>Passiva - {skills.passive.name}</p>
-        <p>{skills.passive.description}</p>
+        <img src={PASSIVE_URL_IMG} alt={skills.passive.name} />
+        <div>
+          <p>Passiva - {skills.passive.name}</p>
+          <p>{skills.passive.description}</p>
+        </div>
       </div>
       <div>
         {skills.spells.map((spell, index) => (
           <section key={spell.name} className='championsSkill__spells'>
-            <h4>
-              {abilityKey[index]} - {spell.name}
-            </h4>
-            <p>{spell.description}</p>
-            <p>
-              Tempo de recarga:{' '}
-              {spell.cooldown.map((item) => `${item}s`).join(' - ')}
-            </p>
-            <p>
-              Custo{skills.resourse && ` de ${skills.resourse}`}:{' '}
-              {spell.cost.join(' - ')}
-            </p>
-            <p>Range: {spell.range.join(' - ')}</p>
+            <img
+              src={`https://ddragon.leagueoflegends.com/cdn/14.4.1/img/spell/${spell.image.full}`}
+              alt={spell.name}
+            />
+            <div>
+              <h4>
+                {abilityKey[index]} - {spell.name}
+              </h4>
+              <p>{spell.description}</p>
+              <p>
+                Tempo de recarga:{' '}
+                {spell.cooldown.map((item) => `${item}s`).join(' - ')}
+              </p>
+              <p>
+                Custo{skills.resourse && ` de ${skills.resourse}`}:{' '}
+                {spell.cost.join(' - ')}
+              </p>
+              <p>Range: {spell.range.join(' - ')}</p>
+            </div>
           </section>
         ))}
       </div>
